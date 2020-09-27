8e8ce1b9e852c195795079ffdafaf296e2f585de
Sankapl Mishra <62585077+sankalpmishra2795@users.noreply.github.com>
2020-09-27 21:59:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Ui from './components/Ui';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Ui />
+      </div>
+    );
+  }
+}
+
+export default App;
